5b2f1d70e3ea04d521a23466896d582e1301282d
PruthviBelgaonkar <80522913+PruthviBelgaonkar@users.noreply.github.com>
2022-10-28 12:51:51 +0530

    Time: 40 ms (12.82%), Space: 12.3 MB (68.68%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    vector<string> removeAnagrams(vector<string>&s) {
-        for(int i=1;i<s.size();i++)
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        vector<vector<string>>ans;
+        
+        for(string s:strs)
         {
-            string x=s[i];
-            sort(begin(x),end(x));
-            string y=s[i-1];
-            sort(begin(y),end(y));
-            if(x==y)
-            {
-                s.erase(s.begin()+i);
-                i--;
-            }
+            string t=s;
+            sort(begin(t),end(t));
+            mp[t].push_back(s);
         }
-        return s;
+        
+        for(auto it:mp)
+        {
+            ans.push_back(it.second);
+        }
+        return ans;
     }
 };
